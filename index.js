a53f15f3517817055204f1f96fd8b27beb8a7289
Edward Angelo Salas <zyro670@gmail.com>
2022-03-19 21:33:30 -0400

    commit tree ffc8bb03cf07dab68e393fc724c3005d81a463cb

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const { interfaces } = require("mocha");
+
+function scuberGreetingForFeet(check) {
+  var msg = 'This one is on me!';
+  if (check < 199)
+  msg = 'This one is on me!';
+  else if (check > 2000 && check < 2500)
+    msg = 'I will gladly take your thirty bucks.';
+  else if (check > 2500)
+    msg = 'No can do.';
+
+  return msg;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  var msg = '';
+  if (city == 'NYC')
+    msg = 'Ok, sounds good.';
+  if (city != 'NYC')
+    msg = "No go.";
+
+  return msg;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(msg) {
+  switch (msg)
+  {
+    case 'generous': msg = "Thank you so much."; break;
+    case 'not as generous': msg = "Thank you."; break;
+    case 'thanks for everything': msg = "Bye."; break;
+  }
+  return msg;
 }
